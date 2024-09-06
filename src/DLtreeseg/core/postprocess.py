4676d896d40ea32d76ba25fcdfb35e90cad3d7b2
jldz9 <jiaweiliwork@outlook.com>
2024-09-05 21:05:03 -0600

    Add TODO

---
+++ src/DLtreeseg/core/postprocess.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Module include post-process functions for DLtreeseg, including image mosaic
+"""
+
+class Postprocess:
+    """
+    post process class to handle images after training is done
+    """
+    # TODO: Add functions that merge tiles back into whole geo-referenced raster  
