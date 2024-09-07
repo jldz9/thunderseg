acf82b87dd7fb1f71602321292e426150ab28863
jldz9 <jiaweiliwork@outlook.com>
2024-09-06 22:49:02 -0600

    Complleted class for generate COCO format

--- src/DLtreeseg/utils/tool.py
+++ src/DLtreeseg/utils/tool.py
@@ -25,10 +25,9 @@ class COCO_format:
     Make COCO Json format (https://cocodataset.org/#home) for 
     """
     def __init__(self, 
-                 metadata: dict = {}):
-        
-        self.info = metadata
-        #self.COCO = {"info": self.info}
+                 info: dict = {}):
+        self.info = info
+        self.COCO = {"info": self.info}
 
 
     @staticmethod
@@ -38,23 +37,23 @@ class COCO_format:
                  annotations_update:list = []):
         
         if len(licenses_update) == 3:
-            licenses = {"url": str(licenses_update[0]), # Optional
-                    "id": int(licenses_update[1]), # License ID, must be unique
-                    "name":str(licenses_update[2]) # Optional
+            licenses = {"id": int(licenses_update[0]), # License ID, must be unique
+                        "name":str(licenses_update[1]), # Optional
+                        "url": str(licenses_update[2]) # Optional           
                     }
             return licenses
         elif len(licenses_update) != 0: 
             raise ValueError(f'licenses section has 3 catagories, the input has {len(licenses_update)}')
 
         if len(images_update) == 8:
-            images = {"license": int(images_update[0]), # Optional
-                    "file_name": str(licenses_update[1]), #Required. Better to be the full path to the image
-                    "coco_url": str(licenses_update[2]), # Optional
-                    "height": int(images_update[3]), # Required
-                    "width": int(images_update[4]), # Required
-                    "date_captured": parser.parse(images_update[5]), # Optional
-                    "flickr_url": str(images_update[6]), # Optional
-                    "id": int(images_update[7]) # Image ID, Required
+            images = {"id": int(images_update[0]), # Image ID, Required
+                      "file_name": str(images_update[1]), #Required. Better to be the full path to the image
+                      "width": int(images_update[2]), # Required
+                      "height": int(images_update[3]), # Required
+                      "license": int(images_update[4]), # Optional
+                      "flickr_url": str(images_update[5]), # Optional                    
+                      "coco_url": str(images_update[6]), # Optional
+                      "date_captured": str(images_update[7]), # Optional         
             }
             return images
         elif len(images_update) != 0: 
@@ -62,26 +61,28 @@ class COCO_format:
         
         if len(categories_update) == 3:
             categories = {
-                "supercategory": str(categories_update[0]), # Optional
-                "id": int(categories_update[1]), # The categories id, must be unique
-                "name": str(categories_update[2])
+                "id": int(categories_update[0]), # The categories id, must be unique
+                "name": str(categories_update[1]),
+                "supercategory": str(categories_update[2]), # Optional
+                
             }
+                
             return categories
         elif len(categories_update) != 0: 
             raise ValueError(f'Categories section has 3 catagories, the input has {len(categories_update)}')
         
         if len(annotations_update) == 10:
             annotations = {
-                "bbox": list(annotations_update[0]), #bounding box, required, default use XYXY absolute coordinate
-                "segmentation": list(annotations_update[1]), # A list of list of points [[x1,y1, x2,y2 ...]] define the shape of the object, optional
-                "area": int(annotations_update[2]), # measured in pixels (10px * 20px = 200px)
-                "iscrowd": int(annotations_update[3]), # specifies whether the segmentation is for a single object (0) or  not possible to delineate the individually from other similar objects. (1)
-                "image_id": int(annotations_update[4]), #corresponds to a specific image in the dataset
-                "category_id": int(annotations_update[5]), # corresponds to a single category specified in the categories section
-                "id": int(annotations_update[6]), # Each annotation also has an id (unique to all other annotations in the dataset)
+                "id": int(annotations_update[0]), # Each annotation also has an id (unique to all other annotations in the dataset)
+                "image_id": int(annotations_update[1]), #corresponds to a specific image in the dataset
+                "category_id": int(annotations_update[2]), # corresponds to a single category specified in the categories section
+                "bbox": list(annotations_update[3]), #bounding box, required, default use XYXY absolute coordinate
+                "area": int(annotations_update[4]), # measured in pixels (10px * 20px = 200px)
+                "iscrowd": int(annotations_update[5]), # specifies whether the segmentation is for a single object (0) or  not possible to delineate the individually from other similar objects. (1)
+                "segmentation": list(annotations_update[6]), # A list of list of points [[x1,y1, x2,y2 ...]] define the shape of the object, optional                
                 "keypoints" : list(annotations_update[7]), #Optional, list of key points with format [x1,y1,v1, ...,xn,yn,vn] visibility (0(no label),1(label, visable),2(label, not visable))
                 "num_keypoints": int(annotations_update[8]), # number of key points
-                "bbox_mode": annotations_update[9], # Bounding box mod, use (x,y) of top left and (x,y) of bottom right, required
+                "bbox_mode": annotations_update[9] # Bounding box mod, use (x,y) of top left and (x,y) of bottom right, required
             } # Each instance should have it's own bbox and segmentation even is in same image and same category
             return annotations
         elif len(annotations_update) != 0: 
@@ -89,93 +90,169 @@ class COCO_format:
 
     
     @staticmethod   
-    def add_license(license_id: list,
+    def _add_licenses(license_id: list,
                     license_url: list =[],
                     license_name: list = []):
         
-        image_count = len(license_id)
-        if len(license_url) != image_count or len(license_name) != image_count:
+        if len(license_url) != len(license_id) or len(license_name) != len(license_id):
             raise AssertionError(f"All input lists should have same length, use 'N/A' or '' in the list for missing value ")
-        
         # Check if any empty lists, if so, fill with default value
         named_vars = {k: v for k, v in locals().items() if k not in ['args', 'kwargs']}
         empty_named_vars = {k: v for k, v in named_vars.items() if v == []}
         if len(empty_named_vars) > 0:
             for key, _ in empty_named_vars.items():
-                new_list = [''] * image_count
-                exec(f'{key} = {new_list}')
-        license_list = [tempdict]
-        for url, id, name in zip(license_url, license_id, license_name):
-            tempdict = COCO_format.template(licenses_update=[url,id,name])
+                new_list = [''] * len(license_id)
+                empty_named_vars[key] = new_list
+        named_vars.update(empty_named_vars)
+        license_list = []
+        for  id, name, url in zip(*list(named_vars.values())):
+            tempdict = COCO_format.template(licenses_update=[id, name, url])
             license_list.append(tempdict)
         return {'licenses':license_list}
 
     @staticmethod
-    def add_images(id: list,
-                   file_name: list ,
-                   license_id: list= [], 
+    def _add_images(id: list,
+                   file_name: list,
+                   width: list ,
+                   height: list ,
+                   license: list= [],
+                   flickr_url: list = [], 
                    coco_url: list = [],
-                   height: list = [],
-                   width: list = [],
-                   date_captured: list= [],
-                   flickr_url: list = []
+                   date_captured: list= [],                   
                    ):
-        if len(id) != len(file_name):
-            raise AssertionError(f"id and filename should have same length")
+        if len(id) != len(file_name) or len(id) != len(width) or len(id) != len(height):
+            raise AssertionError (f'Length of id, fil_name, width, and height should be the same')
         named_vars = {k: v for k, v in locals().items() if k not in ['args', 'kwargs']}
         empty_named_vars = {k: v for k, v in named_vars.items() if v == []}
         if len(empty_named_vars) > 0:
             for key, _ in empty_named_vars.items():
-                if key == 'height' or key == 'width' or key == 'license_id':
+                if key == 'license':
                     new_list = [0] * len(id)
-                elif key == 'data_captured':
-                    new_list = ["19491001-15:43:32"]
+                elif key == 'date_captured':
+                    new_list = ["1949-10-01T07:43:32Z"] * len(id)  
                 else:
                     new_list = [''] * len(id)
-                exec(f'{key} = {new_list}')
+                empty_named_vars[key] = new_list
+        named_vars.update(empty_named_vars)
         image_list = []
-        for i, fn, lid, cocol, h, w, dc, fl in zip(id, file_name, license_id, 
-                                                  coco_url, height, width, 
-                                                  date_captured, flickr_url):
-            tempdict = COCO_format.template(images_update=[lid, fn, cocol, h, w, dc, fl, i])
+        for i, fn, w, h, lic, fl, cocol, dc in zip(*list(named_vars.values())):
+            tempdict = COCO_format.template(images_update=[i, fn, w, h, lic, fl, cocol, dc])
             image_list.append(tempdict)
         return {"images":image_list}
     
     @staticmethod
-    def add_categories(id: list,
+    def _add_categories(id: list,
+                       name: list = [],
                        supercategory:list = [], 
-                       name: list = []):
+                       ):
         named_vars = {k: v for k, v in locals().items() if k not in ['args', 'kwargs']}
         empty_named_vars = {k: v for k, v in named_vars.items() if v == []}
         if len(empty_named_vars) > 0:
             for key, _ in empty_named_vars.items():
                 new_list = [''] * len(id)
-                exec(f'{key} = {new_list}')
+                empty_named_vars[key] = new_list
+            named_vars.update(empty_named_vars)
         categories_list = []
-        for i, s, n in zip(id, supercategory, name):
-            tempdict = COCO_format.template(categories_update=[s, i ,n])
+        for i, n, s in zip(*list(named_vars.values())):
+            tempdict = COCO_format.template(categories_update=[i ,n, s])
             categories_list.append(tempdict)
         return {"categories": categories_list}
-    """
+    
     @staticmethod
-    def add_annotations(bbox:list,
-                        image_id:list, 
-                        category_id:list,
-                        id:list,
-                        segmentation,
-                        area,
-                        iscrowd,
-                        keypoints,
-                        num_keypoints,
+    def _add_annotations(id:list,
+                        image_id:list,
+                        category_id:list, 
+                        bbox:list,                              
+                        area:list,
+                        iscrowd:list,
+                        segmentation:list = [],
+                        keypoints :list = [],
+                        num_keypoints :list = [],
                         bbox_mode = 'xyxy'):
+        if bbox_mode == 'xyxy':
+            bbox_mode = [BoxMode.XYXY_ABS]*len(id)
+        elif bbox_mode == 'xywh':
+            bbox_mode = [BoxMode.XYWH_ABS]*len(id)
+        elif isinstance(bbox_mode, list):
+            bbox_mode = bbox_mode
+        else:
+            raise ValueError('Only "xyxy" ,"xywh" or customized list are currently supported') 
         named_vars = {k: v for k, v in locals().items() if k not in ['args', 'kwargs']}
         empty_named_vars = {k: v for k, v in named_vars.items() if v == []}
         if len(empty_named_vars) > 0:
             for key, _ in empty_named_vars.items():
-                new_list = [''] * len(id)
-                exec(f'{key} = {new_list}')
-        if bboxmode == 'xyxy':
-            bm = BoxMode.XYXY_ABS
-        elif bboxmode == 'xywh':
-            bm = BoxMode.XYWH_ABS
-            """
+                if key == "keypoints" or key == "segmentation":
+                    new_list = [[]] * len(id)
+                elif key == "num_keypoints":
+                    new_list = [0] * len(id)
+                empty_named_vars[key] = new_list
+            named_vars.update(empty_named_vars)
+        annotations_list = []
+        for id, ii, ci, bb,  a, ic, se, kp, nk, bbm in zip(*list(named_vars.values())):
+            tempdict = COCO_format.template(annotations_update=[id, ii, ci, bb, a, ic, se, kp, nk, bbm])
+            annotations_list.append(tempdict)
+        return {"annotations": annotations_list}
+        
+    def add_licenses(self,
+                    license_id: list,
+                    license_url: list =[],
+                    license_name: list = []):
+        licenses = self._add_licenses(license_id, license_url, license_name)
+        if "licenses" in self.COCO:
+            self.COCO['licenses'] = self.COCO['licenses'] + licenses['licenses']
+        else:
+            self.COCO.update(licenses)
+    
+    def add_images(self, id: list,
+                   file_name: list,
+                   width: list ,
+                   height: list ,
+                   license: list= [],
+                   flickr_url: list = [], 
+                   coco_url: list = [],
+                   date_captured: list= [],                   
+                   ):
+        images = self._add_images(id, file_name,  width, height,license, 
+                                                  coco_url,  
+                                                  date_captured, flickr_url)
+        if "images" in self.COCO: 
+            self.COCO['images'] = self.COCO['images'] + images['images']
+        else: 
+            self.COCO.update(images)
+
+    def add_categories(self, id: list,
+                       name: list = [],
+                       supercategory:list = []):
+        categories = self._add_categories(id, name, supercategory)
+        if "categories" in self.COCO:
+            self.COCO["categories"] = self.COCO['categories'] + categories['categories']
+        else: 
+            self.COCO.update(categories)
+
+    
+    def add_annotations(self, id:list,
+                        image_id:list,
+                        category_id:list, 
+                        bbox:list,                              
+                        area:list,
+                        iscrowd:list,
+                        segmentation:list = [],
+                        keypoints :list = [],
+                        num_keypoints :list = [],
+                        bbox_mode = 'xyxy'):
+        annotations = self._add_annotations(id, image_id, category_id, bbox, 
+                                                area, iscrowd, segmentation,
+                                                keypoints,
+                                                num_keypoints,
+                                                bbox_mode)
+        if 'annotations' in self.COCO: 
+            self.COCO['annotations'] = self.COCO['annotations'] + annotations['annotations']
+        else:
+            self.COCO.update(annotations)
+    
+    def COCO(self):
+        return self.COCO
+    
+    def save_json(self, save_path:str):
+        with open(save_path, 'w') as f:
+            json.dump(self.COCO, f)
