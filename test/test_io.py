d164a0bd7e35bf26f886adb23a1487f1b7d7e13e
jldz9 <jiaweiliwork@outlook.com>
2024-09-20 18:45:13 -0600

    test update

--- test/test_io.py
+++ test/test_io.py
@@ -4,15 +4,12 @@ sys.path.append('/home/jldz9/DL/DL_packages/detectree2/detectree2')
 sys.path.append('/home/jldz9/DL/DL_packages/DLtreeseg/src')
 from pathlib import Path
 import glob
-from DLtreeseg.core.io import setup_cfg
 from detectron2.config import get_cfg
 from detectron2 import model_zoo
 from detectron2.engine import DefaultPredictor
-from detectree2.models.predict import predict_on_data
-from detectree2.preprocessing.tiling import tile_data
-from detectree2.models.train import get_tree_dicts, get_filenames
-from detectree2.models.outputs import project_to_geojson, polygon_from_mask, stitch_crowns, clean_crowns
 import rasterio as rio
+import geopandas as gpd 
+"""
 fpath = Path('/home/jldz9/DL/DL_drake/Drake/Ref/Drake20220928_MS.tif')
 data = rio.open(fpath)
 model = Path('/home/jldz9/DL/models/230729_05dates.pth')
@@ -29,4 +26,7 @@ predict_on_data(tiles_path+"/", predictor=DefaultPredictor(cfg))
 project_to_geojson(tiles_path, tiles_path + "predictions/", tiles_path + "predictions_geo/")
 crowns = stitch_crowns(tiles_path + "predictions_geo/", 1)
 print()
-
+"""
+shp_path = Path('/home/jldz9/DL/DL_drake/shp/shurbcrown.shp')
+shp = gpd.read_file(shp_path)
+print()
--- test/test_preprocess.py
+++ test/test_preprocess.py
@@ -1,3 +1,6 @@
+
+import sys
+sys.path.append('/home/jldz9/DL/DL_packages/DLtreeseg/src')
 from pathlib import Path
 import rasterio as rio
 import numpy as np
@@ -9,4 +12,6 @@ fpath = Path('/home/jldz9/DL/DL_drake/Drake/Ref/Drake20220928_MS.tif')
 output_path = Path('/home/jldz9/DL/output')
 
 a = Tile(fpth=fpath, output_path=output_path, buffer_size=20, tile_size=100)
-a.to_gis(Path('/home/jldz9/DL/DL_drake/tiles'))
+a.resample(0.05)
+a.tile_image()
+#a.to_gis(Path('/home/jldz9/DL/DL_drake/tiles'))
