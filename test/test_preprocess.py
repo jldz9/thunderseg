024e2ea9374aa597daf5d8509469cae632584d9b
jldz9 <jiaweiliwork@outlook.com>
2024-09-21 22:14:39 -0600

    test update

--- test/test_preprocess.py
+++ test/test_preprocess.py
@@ -9,10 +9,9 @@ from rasterio.transform import from_origin
 from DLtreeseg.core.preprocess import Tile
 
 fpath = Path('/home/jldz9/DL/DL_drake/Drake/Ref/Drake20220928_MS.tif')
+shp_path = Path('/home/jldz9/DL/DL_drake/shp/shurbcrown.shp')
 output_path = Path('/home/jldz9/DL/output')
 
 a = Tile(fpth=fpath, output_path=output_path, buffer_size=20, tile_size=100)
 a.tile_image()
-a.resample(0.09)
-a.tile_image()
-#a.to_gis(Path('/home/jldz9/DL/DL_drake/tiles'))
+a.tile_shape(shp_path)
