e766165086679ea463761eeb27687b0be734c008
jldz9 <jiaweiliwork@outlook.com>
2024-09-21 22:14:06 -0600

    add tile_shape method, make COCO ready dict

--- src/DLtreeseg/core/preprocess.py
+++ src/DLtreeseg/core/preprocess.py
@@ -8,16 +8,20 @@ import io
 import json
 import sys
 from pathlib import Path
+from datetime import datetime
 
+import geopandas as gpd
 import numpy as np
 import rasterio as rio
 from rasterio.io import DatasetReader, MemoryFile
 from rasterio.windows import Window
 from rasterio.transform import Affine
 from rasterio.enums import Resampling
+from shapely import box
 
-from DLtreeseg.utils import pack_h5_list
+from DLtreeseg.utils import pack_h5_list, to_pixelcoord
 from DLtreeseg.core import save_h5, save_gis
+
 
 class Tile: 
     """Preprocess module for image IO, tilling.
@@ -40,7 +44,7 @@ class Tile:
             debug: Switch to turn on debug
         """
         self.fpth = Path(fpth).absolute()
-        self._dataset = rio.open(self.fpth)
+        
         self._output_path = Path(output_path).absolute()
         self._output_path.mkdir(exist_ok=True)
         self._tile_size = tile_size
@@ -49,6 +53,20 @@ class Tile:
         self._buffer_size = buffer_size
         if not isinstance(self._buffer_size, int) :
             raise TypeError(f"Buffer size should be Int, not {type(self._buffer_size_pixel_x)}")
+        self._images = {'id':[],
+                        "file_name":[],
+                        "width":[],
+                        "height": [],
+                        "date_captured":[]
+        }
+        self._annotations = {'id':[],
+                             'image_id':[],
+                             'category_id':[],
+                             'bbox':[],
+                             'area':[],
+                             'iscrowd':[],
+                             'segmentation':[]} 
+        
         if debug is True:
             print('debug')
 
@@ -56,21 +74,22 @@ class Tile:
         """Make rasterio windows for raster tiles, pad original dataset to makesure all tiles size looks the same.
         Tiles will overlap on right and bottom buffer.         
         """
-        profile = self._dataset.profile.copy()
+        oridataset = rio.open(self.fpth)
+        profile = oridataset.profile.copy()
         y = profile['height']
         x = profile['width']
-        transform = self._dataset.profile['transform']
+        transform = oridataset.profile['transform']
         # Convert meter tile size to pixel tile size
-        tile_size_pixel_x = int(np.ceil(self._tile_size / self._dataset.res[0]))
-        tile_size_pixel_y = int(np.ceil(self._tile_size / self._dataset.res[1]))
-        buffer_size_pixel_x = int(np.ceil(self._buffer_size / self._dataset.res[0]))
-        buffer_size_pixel_y = int(np.ceil(self._buffer_size / self._dataset.res[1]))
+        tile_size_pixel_x = int(np.ceil(self._tile_size / oridataset.res[0]))
+        tile_size_pixel_y = int(np.ceil(self._tile_size / oridataset.res[1]))
+        buffer_size_pixel_x = int(np.ceil(self._buffer_size / oridataset.res[0]))
+        buffer_size_pixel_y = int(np.ceil(self._buffer_size / oridataset.res[1]))
         # Calculate number of tiles along height and width with buffer.
         n_tiles_x = int(np.ceil((x + buffer_size_pixel_x) / (buffer_size_pixel_x + tile_size_pixel_x)))
         n_tiles_y = int(np.ceil((y + buffer_size_pixel_y) / (buffer_size_pixel_y + tile_size_pixel_y)))
         
         # Add buffer to original raster to make sure every tiles has same size.
-        data = self._dataset.read()
+        data = oridataset.read()
         data = np.where(data<0, 0, data)
         pad = ((0,0),
                 (buffer_size_pixel_y, n_tiles_y * (tile_size_pixel_y + buffer_size_pixel_y) - y),
@@ -99,7 +118,7 @@ class Tile:
             ) 
             for start_x, start_y in zip(flat_tile_x, flat_tile_y)
             ])
-        self._dataset.close()
+        oridataset.close()
         memfile = MemoryFile()
         with memfile.open(**profile) as dst:
             dst.write(padded_data)
@@ -140,7 +159,6 @@ class Tile:
         self._get_window()
         tiles_list = []
         self._profiles = []
-        self._coco_dicts = []
         num_tiles = len(self._windows)
         for idx, window in enumerate(self._windows):
             tile_profile = self._dataset.profile
@@ -152,8 +170,15 @@ class Tile:
         })
             tiles_list.append(tile_data)
             self._profiles.append(tile_profile)
-            sys.stdout.write(f'\rWorking on: {idx+1}/{num_tiles} tile')
+            sys.stdout.write(f'\rWorking on: {idx+1}/{num_tiles} image tile')
             sys.stdout.flush()
+            self._images['id'].append(idx+1)
+            filename = f'{self._output_path}/{self.fpth.stem}_{window.row_off}_{window.col_off}{self.fpth.suffix}'
+            self._images['file_name'].append(filename)
+            self._images['width'].append(window.width)
+            self._images['height'].append(window.height)
+            self._images['date_captured'].append(datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ'))
+            save_gis(filename, tile_data, tile_profile)
         print()
         self._stack_tiles = np.stack(tiles_list, axis=0)
         self._report = {'file name': self.fpth.name,
@@ -165,7 +190,49 @@ class Tile:
                 'crs': str(self._dataset.crs),
                 'band': self._dataset.count
                 }
+        
     
+    def tile_shape(self, shp_path: str):
+        """Use raster window defined by _get_window to tile input shapefiles.
+        Args:
+            shp_path: Path to single shapefile
+        """
+        if not hasattr(self, "_windows"):
+            self._get_window()
+        nw_conor_coor = (self._dataset.bounds.left, self._dataset.bounds.top)
+        self._shp_path = Path(shp_path)
+        self._shpdataset = gpd.read_file(self._shp_path)
+        if self._shpdataset.crs.to_epsg()!= self._dataset.crs.to_epsg():
+            self._shpdataset.to_crs(epsg=self._dataset.crs.to_epsg())
+        annotation_id = 1
+        for idx, window in enumerate(self._windows):
+            sys.stdout.write(f'\rClipping shapfile for: {idx+1}/{len(self._windows)} tile')
+            sys.stdout.flush()
+            geobounds = self._dataset.window_bounds(window)
+            bbox = box(*geobounds)
+            window_gdf = gpd.GeoDataFrame(geometry=[bbox], crs=self._dataset.crs.to_epsg())
+            intersection = gpd.overlay(self._shpdataset, window_gdf, how='intersection')
+            segmentations = intersection.geometry.tolist()
+            if len(segmentations) > 0:
+                for seg in segmentations:
+                    pixel_coord = to_pixelcoord(self._dataset.transform, window, seg)
+                    area = seg.area/(self._dataset.res[0]*self._dataset.res[1])
+                    bbox = [min(pixel_coord[0::2]), 
+                            min(pixel_coord[1::2]), 
+                            max(pixel_coord[0::2]) - min(pixel_coord[0::2]),
+                            max(pixel_coord[1::2]) - min(pixel_coord[1::2])]
+                    self._annotations['id'].append(annotation_id)
+                    annotation_id += 1
+                    self._annotations['image_id'].append(idx+1)
+                    self._annotations['category_id'].append(1)
+                    self._annotations['bbox'].append(bbox)
+                    self._annotations['area'].append(area)
+                    self._annotations['iscrowd'].append(0)
+                    self._annotations['segmentation'].append(pixel_coord)
+
+            
+            
+
     @property
     def data(self):
         return self._stack_tiles
