c60d7c2b29e2fc8183c4d802c83221348024241c
jldz9 <jiaweiliwork@outlook.com>
2024-09-28 19:04:31 -0600

    init

--- src/DLtreeseg/core/train.py
+++ src/DLtreeseg/core/train.py
@@ -6,5 +6,10 @@ Model training related module in DLtreeseg
 """
 
 import sys
+from pathlib import Path
 
-import torch
+import torch, detectron2
+from detectron2.model_zoo import get_config
+from detectron2.config import LazyConfig, instantiate
+from detectron2.config import LazyCall as L
+
