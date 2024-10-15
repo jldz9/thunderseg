bbeb08934be9bb890dac5389d8030ea03896773b
jldz9 <jiaweiliwork@outlook.com>
2024-10-15 02:14:35 +0000

    updates

--- test/test_preprocess.py
+++ test/test_preprocess.py
@@ -1,22 +1,21 @@
 
 import os
-os.environ.get('PROJ_LIB')
-os.environ['PROJ_LIB'] = '/home/jldz9/miniconda3/envs/DL/share/proj'
 import sys
-sys.path.append('/home/jldz9/DL/DL_packages/DLtreeseg/src')
 from pathlib import Path
 import rasterio as rio
 import numpy as np
 from rasterio.windows import Window, transform
 from rasterio.transform import from_origin
+sys.path.append((Path(__file__).parents[1] / 'src').as_posix())
 from DLtreeseg.core.preprocess import Tile
 
-fpath = Path('/home/jldz9/DL/DL_drake/Drake/Ref/Drake20220928_MS.tif')
-shp_path = Path('/home/jldz9/DL/DL_drake/shp/shurbcrown.shp')
-output_path = Path('/home/jldz9/DL/output')
+
+fpath = Path('/home/vscode/remotehome/DL_drake/Drake/Ref/Drake20220928_MS.tif')
+shp_path = Path('/home/vscode/remotehome/DL_drake/shp/shurbcrown_train.shp')
+output_path = Path('/home/vscode/remotehome/DL_drake/output')
 
 a = Tile(fpth=fpath, output_path=output_path, buffer_size=20, tile_size=100)
 a.tile_image()
 a.tile_shape(shp_path)
-a.to_COCO('/home/jldz9/DL/output/shurbcoco.json', 'shurbcoco')
+a.to_COCO('/home/vscode/remotehome/DL_drake/shurbcoco.json')
 print()
