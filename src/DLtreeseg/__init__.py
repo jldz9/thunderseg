1ce2371326d99d8915115a8f504c9e0b749622f7
jldz9 <jiaweiliwork@outlook.com>
2024-10-15 02:12:35 +0000

    Remove train for now

--- src/DLtreeseg/__init__.py
+++ src/DLtreeseg/__init__.py
@@ -1,3 +1,3 @@
-from DLtreeseg.core import io, preprocess, postprocess, train
+from DLtreeseg.core import io, preprocess, postprocess
 from DLtreeseg.utils import tool
 from DLtreeseg._version import __version__
