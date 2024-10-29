a80d90484dc7f557ddc896ab2821938aecc6cf4a
jldz9 <jiaweiliwork@outlook.com>
2024-10-29 18:41:59 +0000

    Add modules

--- src/DLtreeseg/__init__.py
+++ src/DLtreeseg/__init__.py
@@ -1,3 +1,4 @@
-from DLtreeseg.core import io, preprocess, postprocess
-from DLtreeseg.utils import tool
+from DLtreeseg.core import io, preprocess, postprocess, train
+from DLtreeseg.utils import tool, debug
 from DLtreeseg._version import __version__
+from DLtreeseg.model import mask_rcnn_R_101_FPN_3x
