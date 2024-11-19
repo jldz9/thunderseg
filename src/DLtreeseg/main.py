ef2aa260f689f16f60ad705b37962b2356b4df6d
jldz9 <jiaweiliwork@outlook.com>
2024-11-19 04:41:23 +0000

    update config file and preprocess workflow

--- src/DLtreeseg/main.py
+++ src/DLtreeseg/main.py
@@ -2,17 +2,20 @@ import argparse
 import sys
 import shutil
 from pathlib import Path
-from types import SimpleNamespace
 
-from DLtreeseg.core import Tile, create_project_structure
-from DLtreeseg.utils import  read_toml
+from colorama import Fore, init
+init(autoreset=True)
+import geopandas as gpd
+import pandas as pd
+from DLtreeseg.core import Tile, create_project_structure, read_config, write_config
+from DLtreeseg.utils import merge_coco
 from DLtreeseg._version import __version__
 
 
 def create_parser():
     synopsis = 'This is a python interface for DLtreeseg program'
     name = 'DLtreeseg'
-    parser = argparse.ArgumentParser(name, description=synopsis)
+    parser = argparse.ArgumentParser(name, description=synopsis, add_help=False)
     parser.add_argument("-v", "--version", action='version', version=f'DLtreeseg {__version__}')
     parser.add_argument('-c', '--config', metavar='PATH', help='Path to config for automate process')
     parser.add_argument('--init', metavar='PATH', help='Initiliza the working dir')
@@ -21,32 +24,47 @@ def create_parser():
     preprocess.add_argument('-c', '--config',metavar='PATH', help='Path to config')
     train = subparser.add_parser('train', help='Preprocess raster into tiles and register dataset')
     train.add_argument('-c', '--config', metavar='PATH', help='Path to config')
-    return parser
+    return parser, preprocess, train
 
-def preprocess(cfg):
-    ori_train_rasters = list(Path(cfg.io.train_rastrer_dir).glob('*.tiff')) + list(Path(cfg.io.train_rastrer_dir).glob('*.tif'))
-    ori_train_shps = list(Path(cfg.io.shape_file_dir).glob('*.shp'))
+def preprocess_step(cfg):
+    ori_train_rasters = list(Path(cfg['io']['train_raster_dir']).glob('*.tiff')) + list(Path(cfg['io']['train_raster_dir']).glob('*.tif'))
+    assert len(ori_train_rasters) > 0, f"Not able to find any rasters under {cfg['io']['train_shp_dir']}"
+    ori_train_shps = list(Path(cfg['io']['train_shp_dir']).glob('*.shp')) #TODO might need to support more formats
+    assert len(ori_train_shps) > 0, f"Not able to find any shapefiles under {cfg['io']['train_shp_dir']}"
+    shps = [gpd.read_file(shp) for shp in ori_train_shps]
+    merged_shp = pd.concat(shps, ignore_index=True)
+    merged_shp.drop_duplicates(inplace=True)
+    merged_shp.to_file(cfg['io']['train_shp']+'/train_shp.shp')
+    coco_list = []
     for r in ori_train_rasters:
-        a = Tile(fpth=r, output_path=cfg.train, buffer_size=cfg.preprocess.buffer_size, tile_size=cfg.preprocess.tile_size)
-        a.tile_image()
-        for s in ori_train_shps:
-            a.tile_shape(s)
-        a.to_COCO(cfg.annotations / 'train_coco.json', 'train_coco', **cfg.preprocess.COCO_info.__dict__)
-    return
+        print(f'{Fore.GREEN}Processing {r.stem}')
+        a = Tile(fpth=r, output_path=cfg['io']['train'], buffer_size=cfg['preprocess']['buffer_size'], tile_size=cfg['preprocess']['tile_size'])
+        a.tile_image(mode=cfg['preprocess']['mode'], shp_path=cfg['io']['train_shp']+'/train_shp.shp')
+        coco_path = a.to_COCO(cfg['io']['annotations'] + f'/{r.stem}_coco.json', **cfg['preprocess']['COCO_info'])
+        coco_list.append(coco_path)
+    merge_coco(coco_list, cfg['io']['annotations']+'/merged_coco.json')
+   
 def main(iargs=None):
     _default_cfg_path = Path(__file__).parent / 'utils/config.toml'
-    _default_cfg = read_toml(_default_cfg_path)
-    parser = create_parser()
+    _default_cfg = read_config(_default_cfg_path)
+    parser, preprocess_parser, train_parser = create_parser()
     inps = parser.parse_args(args=iargs)
+    if len(iargs) <1:
+        parser.print_help()
+        sys.exit(1)
     if inps.init:
-        ns = create_project_structure(inps.init)
         shutil.copy(_default_cfg_path, inps.init)
-        _default_cfg.append(ns)
+        print(f'{Fore.GREEN} Config copied under {Path(inps.init).resolve()}')
     if inps.step == 'preprocess':   
-        cfg = _default_cfg.append(read_toml(inps.config))
-        preprocess(cfg)
+        if inps.config is None:
+            preprocess_parser.print_help()
+            sys.exit(1)
+        cfg = read_config(inps.config)
+        directories = create_project_structure(cfg['io']['workdir'])
+        cfg['io'].update(directories)
+        write_config(cfg, inps.config)
+        preprocess_step(cfg)
     if inps.step == 'train':
-        cfg = _default_cfg.append(read_toml(inps.config))
-        train_cfg = () 
+        train_cfg = []
 if __name__ == '__main__':
     main(sys.argv[1:])
