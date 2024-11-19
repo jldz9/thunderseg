f5a17d0ae81b9aa0e5a7f691c7349247a082a97f
jldz9 <jiaweiliwork@outlook.com>
2024-11-19 04:45:10 +0000

    test update

--- test/test_preprocess.py
+++ test/test_preprocess.py
@@ -9,19 +9,21 @@ from rasterio.transform import from_origin
 sys.path.append((Path(__file__).parents[1] / 'src').as_posix())
 from DLtreeseg.core.preprocess import Tile
 from DLtreeseg.utils import merge_coco
+from pycocotools.coco import COCO
 
 fpath = Path('/home/vscode/remotehome/DL_drake/Drake/Ref/Drake20220928_MS.tif')
-shp_path = Path('/home/vscode/remotehome/DL_drake/shp/shurbcrown_train.shp')
+shp_path = Path('/home/vscode/remotehome/DL_drake/shp/shp_20220928.shp')
 output_path = Path('/home/vscode/remotehome/DL_drake/output')
-"""
-a = Tile(fpth=fpath, output_path=output_path, buffer_size=20, tile_size=100)
+
+a = Tile(fpth=fpath, output_path=output_path, buffer_size=0, tile_size=100)
 a.tile_image()
 a.tile_shape(shp_path)
-a.to_COCO('/home/vscode/remotehome/DL_drake/shurbcoco.json')
+a.to_COCO('/home/vscode/remotehome/DL_drake/demo.json')
 print()
-"""
-coco1 = '/home/vscode/remotehome/DL_drake/shurbcoco.json'
-coco2 = '/home/vscode/remotehome/DL_drake/Drake20220928_MS_coco.json'
+
+
+#coco1 = '/home/vscode/remotehome/DL_drake/shurbcoco.json'
+#coco2 = '/home/vscode/remotehome/DL_drake/Drake20220928_MS_coco.json'
 #coco3 = '/home/vscode/remotehome/DL_drake/Drake20220928_MS_row5742_col5742_coco.json'
 
-merge_coco([coco1, coco2],'/home/vscode/remotehome/DL_drake/combined.json')
+#merge_coco([coco1, coco2],'/home/vscode/remotehome/DL_drake/combined.json')
