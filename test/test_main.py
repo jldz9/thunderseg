dc5ca1c20b10639d3199ad876f066af7ff8b7611
jldz9 <jiaweiliwork@outlook.com>
2024-11-22 23:32:47 +0000

    test update

---
+++ test/test_main.py
@@ -0,0 +1,6 @@
+import sys
+sys.path.append('/home/vscode/remotehome/DL_packages/DLtreeseg/src')
+
+from DLtreeseg import main
+
+main.main(['preprocess', '-c', '/workspaces/DLtreeseg/test/config.toml'])
