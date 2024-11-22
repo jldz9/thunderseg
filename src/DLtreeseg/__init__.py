8b7f70d291568cc0b7fe1a85b5b164e7fe42d108
jldz9 <jiaweiliwork@outlook.com>
2024-11-22 23:29:52 +0000

    Remove io module

--- src/DLtreeseg/__init__.py
+++ src/DLtreeseg/__init__.py
@@ -1,4 +1,5 @@
-from DLtreeseg.core import io, preprocess, postprocess, train
+from DLtreeseg.core import preprocess, postprocess, train
 from DLtreeseg.utils import tool, debug
 from DLtreeseg._version import __version__
 from DLtreeseg.model import mask_rcnn_R_101_FPN_3x
+from DLtreeseg import main
--- src/DLtreeseg/core/__init__.py
+++ src/DLtreeseg/core/__init__.py
@@ -1,4 +1,3 @@
-from .io import *
 from .preprocess import *
 from .train import *
 from .postprocess import *
