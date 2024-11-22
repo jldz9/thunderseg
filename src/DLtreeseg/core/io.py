af0041c5a8fe6495c4a5fe7f9a7fbdc77a85228c
jldz9 <jiaweiliwork@outlook.com>
2024-11-22 21:12:09 +0000

    Remove, read_json, save_h5 method and combine  read_config, write_config into tool.Config class

--- src/DLtreeseg/core/io.py
+++ src/DLtreeseg/core/io.py
@@ -27,74 +27,19 @@ def create_project_structure(workdir: str) -> dict:
     """
     workdir = Path(workdir).resolve()
     directories = dict(
-            annotations = workdir / "datasets" / "annotations",
-            train = workdir / "datasets" / "train",
-            train_shp = workdir / "datasets" / "train" / "shp",
-            val = workdir / "datasets" / "val",
-            val_shp = workdir / "datasets" / "val" / "shp",
-            test = workdir / "datasets" / "test",
-            result = workdir / "results"
+            ANNOTATIONS = workdir / "datasets" / "annotations",
+            TRAIN = workdir / "datasets" / "train",
+            TRAIN_SHP = workdir / "datasets" / "train" / "shp",
+            VAL = workdir / "datasets" / "val",
+            VAL_SHP = workdir / "datasets" / "val" / "shp",
+            PREDICT = workdir / "datasets" / "predict",
+            RESULT = workdir / "results",
+            TEMP = workdir / "temp"
     )
     for key, directory in directories.items():
         directory.mkdir(parents=True, exist_ok=True)
         print(f"Created {key} folder at {directory}")
     return {k:v.as_posix() for k, v in directories.items()}
-
-def read_config(config_path) -> dict:
-    """Read toml config file used by DLtreeseg"""
-    with open(config_path, 'rb') as f:
-        toml = tomllib.load(f)
-    return toml
-
-def read_json(json_path: str):
-        with open(json_path, 'r') as f:
-            COCO = json.load(f)
-        return COCO
-
-def save_h5(save_path:str, data:np.ndarray = [], attrs:dict = None, **kwarg):
-    """
-    Use of h5py to storage data to local disk. **kwarg should contains packed binary data from
-    function pack_h5_list.
-    """
-    save_path = Path(save_path)
-    if save_path.is_file():
-        while True:
-            rmfile = input(f'File {save_path.name} exist, do you want to open this dataset? Y(es)/O(verwrite)/C(ancel) ')
-            if rmfile.lower() == 'y':
-                break
-            if rmfile.lower() == 'o':
-                save_path.unlink()
-                break
-            if rmfile.lower() == 'c':
-                sys.exit()
-    with h5py.File(save_path, 'r+') as hf:
-        if save_path.stem in hf:
-            while True:
-                rmgroup = input(f'Group {save_path.stem} exist, do you want to overwrite? Y(es)/N(o) ')
-                if rmgroup.lower() == 'y':
-                    del hf[save_path.stem]
-                    break
-                if rmgroup.lower() == 'n':
-                    sys.exit()
-        grp = hf.create_group(save_path.stem)
-        grp.create_dataset('data', data=data)
-        if attrs is not None:
-            for key, value in attrs.items():
-                grp.attrs[key] = value
-        if kwarg:
-            for key, value in kwarg.items():
-                grp.create_dataset(key, data=value)
-    print(f'{save_path} saved!')
-
-def to_file(path_to_file:str, data:np.ndarray, profile=None, mode:str='bgr'):
-    path_to_file = Path(path_to_file)
-    if mode.lower() == 'bgr':
-        with rio.open(path_to_file,'w', **profile) as dst:
-            for i in range(0,3):
-                dst.write(data[i], i+1)
-    if mode.lower() == 'ms':
-        with rio.open(path_to_file, 'w', **profile) as dst:
-            dst.write(data)
 
 def to_png(data: np.ndarray, path_to_file:str):
         band1 = data[0] # B
@@ -110,7 +55,3 @@ def to_png(data: np.ndarray, path_to_file:str):
         
         array_bgr = cv2.cvtColor(normalized_array, cv2.COLOR_RGB2BGR)
         cv2.imwrite(path_to_file, array_bgr)
-
-def write_config(data, write_path):
-    with open(write_path,'wb') as f:
-        tomli_w.dump(data, f)
