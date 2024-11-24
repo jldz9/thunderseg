413953f60ce6acd83e13d6add6102dcb9f60ca6e
jldz9 <jiaweiliwork@outlook.com>
2024-11-24 03:21:19 +0000

    Update image plot fit image and target

--- src/DLtreeseg/utils/debug.py
+++ src/DLtreeseg/utils/debug.py
@@ -5,7 +5,7 @@ import numpy as np
 from matplotlib.patches import Rectangle
 
 
-def check_image_target(image, target, savepath=None):
+def check_image_target(image, target=None, savepath=None):
     """ Plot a single image with target to check the quality of transform
     Args: 
         image: (torch.Tensor, torchvision.tv_tensors._image.Image, or np.array) the single image with an array-like format
@@ -14,27 +14,27 @@ def check_image_target(image, target, savepath=None):
     if not isinstance(image, np.ndarray):
         image = image.cpu().permute(1,2,0).numpy()
         image = (image - np.min(image))/(np.max(image) - np.min(image))
-    target = {k: v.cpu().numpy() for k, v in target.items()}
+    
     plt.figure(figsize=(10,10))
     plt.imshow(image)
-    ax = plt.gca()
-
-    for box, label, mask in zip(target['boxes'], target['labels'], target['masks']):
-        x_min, y_min, x_max, y_max = box
-        width = x_max - x_min
-        height = y_max - y_min
-        rect = Rectangle((x_min, y_min), width, height, linewidth=2, edgecolor='r', facecolor='none')
-        ax.add_patch(rect)
-        plt.text(x_min, y_min, f'Label: {label}', color='white', fontsize=12, backgroundcolor='red')
-        masked_image = np.ma.masked_where(mask == 0, mask)
-        masked_image = np.transpose(masked_image, (1,2,0))
-        plt.imshow(masked_image, cmap='jet', alpha=0.5)
     plt.axis('off')
-
+    ax = plt.gca()
+    if target is not None:
+        target = {k: v.cpu().numpy() if isinstance(v, torch.Tensor) else v for k, v in target.items()}
+        for box, label, mask, area in zip(target['boxes'], target['labels'], target['masks'], target['area']):
+            x_min, y_min, x_max, y_max = box
+            width = x_max - x_min
+            height = y_max - y_min
+            rect = Rectangle((x_min, y_min), width, height, linewidth=0.5, edgecolor='r', facecolor='none')
+            ax.add_patch(rect)
+            #fontsize = int(np.sqrt(area))//3
+            #plt.text(x_min, y_min, f'Label: {label}', color='white', fontsize=fontsize, backgroundcolor='yellow')
+            masked_image = np.ma.masked_where(mask == 0, mask)
+            #masked_image = np.transpose(masked_image, (1,2,0))
+            plt.imshow(masked_image, cmap='jet', alpha=0.5)
     if savepath is not None: 
         plt.savefig(savepath, bbox_inches='tight',pad_inches=0)
         
-
 
 
 
