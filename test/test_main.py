2f5504a9a5a0e39a4b5926e9fb9ecdb3f61f156e
jldz9 <jiaweiliwork@outlook.com>
2024-11-25 20:56:20 +0000

    test update

--- test/test_main.py
+++ test/test_main.py
@@ -3,8 +3,9 @@ import time
 sys.path.append('/home/vscode/remotehome/DL_packages/DLtreeseg/src')
 
 from DLtreeseg import main
-#main.main(['--init', '/workspaces/DLtreeseg/test'])
-#main.main(['preprocess', '-c', '/workspaces/DLtreeseg/test'])
+main.main(['--init', '/home/vscode/remotehome/DL_drake/output'])
+main.main(['preprocess', '-c', '/home/vscode/remotehome/DL_drake/output'])
 #time.sleep(5)
-main.main(['train', '-c', '/workspaces/DLtreeseg/test/config.toml'])
+#main.main(['train', '-c', '/home/vscode/remotehome/DL_drake/output'])
 #TODO Bug? When run sequently has Unexpected segmentation fault encountered in worker error, but if run only trian does not 
+main.main(['predict', '-c', '/home/vscode/remotehome/DL_drake/output'])
