2b2bd44664f05321745339bb285b3b0b433d5d90
jldz9 <jiaweiliwork@outlook.com>
2025-01-20 17:27:42 -0700

    Reorganize argparser

--- src/thunderseg/main.py
+++ src/thunderseg/main.py
@@ -24,19 +24,34 @@ from thunderseg._version import __version__
 def create_parser():
     synopsis = 'This is a python interface for thunderseg program'
     name = 'thunderseg'
-    parser = argparse.ArgumentParser(name, description=synopsis, add_help=False)
+    parser = argparse.ArgumentParser(name, description=synopsis, add_help=True)
     parser.add_argument("-v", "--version", action='version', version=f'thunderseg {__version__}')
-    parser.add_argument('-c', '--config', metavar='PATH', help='Path to config for automate process')
-    parser.add_argument('--init', metavar='PATH', help='Initiliza the working dir')
+    parser.add_argument('-g', '--get_config', metavar='PATH', help='Get the default config file to given path')
     subparser = parser.add_subparsers(dest='step')
+    
     preprocess = subparser.add_parser('preprocess', help='Preprocess raster into tiles and register dataset')
     preprocess.add_argument('-c', '--config',metavar='PATH', help='Path to config')
+    preprocess.set_defaults(func=preprocess_step)
+
     train = subparser.add_parser('train', help='Train models')
     train.add_argument('-c', '--config', metavar='PATH', help='Path to config')
+    train.set_defaults(func=train_step)
+
     predict = subparser.add_parser('predict', help='Predict using trained model')
     predict.add_argument('-c','--config', metavar='PATH', help='Path to config')
     predict.add_argument('--ckpt', metavar='PATH', help='The saved checkpoint path, if not provided will try to search the latest one')
-    return parser, preprocess, train, predict
+    predict.set_defaults(func=predict_step)
+    return parser
+
+def check_args(func):
+    """Decorator to check if the arguments are correct
+    """
+    def wrapper(args):
+        if args.config is None:
+            print(f'{Fore.RED}A valid PATH is required after -c or --config, use --help for more information')
+            sys.exit(1)
+        return func(args)
+    return wrapper
 
 def find_config(config):
     cfg_path = Path(config)
@@ -49,10 +64,15 @@ def find_config(config):
         return search_cfg[0].resolve()
     else:
         return cfg_path.resolve()
-
-def preprocess_step(cfg):
+    
+@check_args
+def preprocess_step(args):
     """Step to prepare everything for project loop
     """
+    cfg_path = find_config(args.config)
+    cfg = Config(config_path=cfg_path)
+    directories = create_project_structure(cfg.IO.WORKDIR)
+    cfg.IO.update(directories)
     train_rasters = list(Path(cfg.IO.TRAIN_RASTER_DIR).glob('*.tiff')) + list(Path(cfg.IO.TRAIN_RASTER_DIR).glob('*.tif'))
     assert len(train_rasters) > 0, f"Not able to find any rasters under {cfg.IO.TRAIN_SHP_DIR}"
     train_shps = list(Path(cfg.IO.TRAIN_SHP_DIR).glob('*.shp')) #TODO might need to support more formats
@@ -69,9 +89,12 @@ def preprocess_step(cfg):
         coco_path = train_r.to_COCO(cfg.IO.TEMP + f'/{r.stem}_coco.json', **cfg.PREPROCESS.COCO_INFO.to_dict())
         train_coco_list.append(coco_path)
     merge_coco(train_coco_list, cfg.IO.ANNOTATIONS+'/train_coco.json')
-    return cfg
+    cfg.to_file(cfg_path)
 
-def train_step(cfg, customize_transform=None): 
+@check_args
+def train_step(args): 
+    cfg_path = find_config(args.config)
+    cfg = Config(config_path=cfg_path)
     coco_train = COCO(cfg.IO.ANNOTATIONS+'/train_coco.json')
     if cfg.TRAIN.MODEL == 'maskrcnn_rgb':
         train = Train(maskrcnn_rgb.MaskRCNN_RGB,
@@ -86,8 +109,22 @@ def train_step(cfg, customize_transform=None):
                       save_path = cfg.IO.RESULT,
                                                   )# TODO add path to checkpointpath and also add callbacks
         train.fit()
-    return cfg
-def predict_step(cfg):
+    cfg.to_file(cfg_path)
+
+@check_args
+def predict_step(args):
+    cfg_path = find_config(args.config)
+    cfg = Config(config_path=cfg_path)
+    
+    # If checkpoint is not provided, try to find the latest one
+    if args.ckpt is not None:
+            cfg.IO.CHECKPOINT = args.ckpt
+    else:
+            ckpt_paths = list(Path(cfg.IO.RESULT).rglob('*.ckpt'))
+            assert ckpt_paths, f'not able to find any checkpoint output under {cfg.IO.RESULT}'
+            latest_ckpt = max(ckpt_paths, key=lambda f: f.stat().st_mtime)
+            cfg.IO.CHECKPOINT = latest_ckpt.resolve().as_posix()
+
     assert Path(cfg.IO.ANNOTATIONS+'/train_coco.json').is_file, f"Cannot find {cfg.IO.ANNOTATIONS+'/train_coco.json'}, did you run preprocess step?"
     coco_train = COCO(cfg.IO.ANNOTATIONS+'/train_coco.json')
     if Path(cfg.IO.ANNOTATIONS+'/predict_coco.json').is_file():
@@ -121,52 +158,23 @@ def predict_step(cfg):
         result = predict.predict() 
         postprocess = Postprocess(predict_coco, result, cfg.IO.RESULT)
         postprocess.mask_rcnn_postprocess()
-        return cfg
+    cfg.to_file(cfg_path)
         
-def main(iargs=None):
+def main():
     _default_cfg_path = Path(__file__).parent / 'utils/config.toml'
-    parser, preprocess_parser, train_parser, predict_parser = create_parser()
-    inps = parser.parse_args(args=iargs)
-    if len(iargs) <1:
+    parser = create_parser()
+    args = parser.parse_args()
+    if len(sys.argv) == 1:
         parser.print_help()
         sys.exit(1)
-    if inps.init:
-        Path(inps.init).mkdir(exist_ok=True, parents=True)
-        shutil.copy(_default_cfg_path, inps.init)
-        print(f'{Fore.GREEN}Config copied under {Path(inps.init).resolve()}')
-    if inps.step == 'preprocess':   
-        if inps.config is None:
-            preprocess_parser.print_help()
-            sys.exit(1)
-        cfg_path = find_config(inps.config)
-        cfg = Config(config_path=cfg_path)
-        directories = create_project_structure(cfg.IO.WORKDIR)
-        cfg.IO.update(directories)
-        cfg = preprocess_step(cfg)
-        cfg.to_file(cfg_path)
-    if inps.step == 'train': #TODO add support of customize_transform
-        if inps.config is None:
-            train_parser.print_help()
-            sys.exit(1)
-        cfg_path = find_config(inps.config)
-        cfg = Config(config_path=cfg_path)
-        cfg = train_step(cfg)
-        cfg.to_file(cfg_path)
-    if inps.step == 'predict':
-        if inps.config is None:
-            predict_parser.print_help()
-            sys.exit(1)
-        cfg_path = find_config(inps.config)
-        cfg = Config(config_path=cfg_path)
-        if inps.ckpt is not None:
-            cfg.IO.CHECKPOINT = inps.ckpt
-        else:
-            ckpt_paths = list(Path(cfg.IO.RESULT).rglob('*.ckpt'))
-            assert ckpt_paths, f'not able to find any checkpoint output under {cfg.IO.RESULT}'
-            latest_ckpt = max(ckpt_paths, key=lambda f: f.stat().st_mtime)
-            cfg.IO.CHECKPOINT = latest_ckpt.resolve().as_posix()
-        cfg = predict_step(cfg)
-        cfg.to_file(cfg_path)
 
+    if args.get_config:
+        Path(args.get_config).mkdir(exist_ok=True, parents=True)
+        shutil.copy(_default_cfg_path, args.get_config)
+        print(f'{Fore.GREEN}Config copied under {Path(args.get_config).resolve()}')
+        sys.exit(0)
+    
+    args.func(args)
+    
 if __name__ == '__main__':
-    main(sys.argv[1:])
+    main()
