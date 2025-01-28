ad9f0323a53cdf10822855084ccba95c21f8a4e4
jldz9 <jiaweiliwork@outlook.com>
2025-01-27 19:09:05 -0700

    test_update

--- test/test_main.py
+++ test/test_main.py
@@ -3,9 +3,11 @@ import time
 sys.path.append('/home/jldz9/DL/DL_packages/thunderseg/src')
 
 from thunderseg import main
-main.main(['--init', '/home/jldz9/DL/DL_drake/output'])
-main.main(['preprocess', '-c', '/home/jldz9/DL/config.toml'])
-#time.sleep(5)
-main.main(['train', '-c', '/home/jldz9/DL/config.toml'])
-#TODO Bug? When run sequently has Unexpected segmentation fault encountered in worker error, but if run only trian does not 
-main.main(['predict', '-c', '/home/jldz9/DL/config.toml'])
+from types import SimpleNamespace
+
+args = SimpleNamespace()
+args.config = '/home/jldz9/thunderseg_example_data/config.toml'
+
+#main.preprocess_step(args)
+#main.train_step(args)
+main.predict_step(args)
